cfa38ac3a41b64a2cc892014d09066990796ca54
Rowan Littlefield <rowanlittlefield@ymail.com>
2018-08-13 15:06:24 -0400

    Refactor todo app into functional component

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -3,20 +3,14 @@ import { Provider } from 'react-redux';
 import NewTodoFormContainer from '../containers/new_todo_form_container';
 import TodoListContainer from '../containers/todo_list_container';
 
-class Todo extends React.Component {
-
-  render() {
-    return (
-      <Provider store={ this.props.store }>
-        <span className="todo-app">
-          <h2>To-Do</h2>
-          <NewTodoFormContainer />
-          <TodoListContainer />
-        </span>
-      </Provider>
-
-    );
-  }
-}
+const Todo = ({store}) => (
+  <Provider store={ store }>
+    <span className="todo-app">
+      <h2>To-Do</h2>
+      <NewTodoFormContainer />
+      <TodoListContainer />
+    </span>
+  </Provider>
+);
 
 export default Todo;
